a3850c7b2c9d615beca9d75231a9843f26ff4480
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2014-11-13 14:46:33 -0500

    remove main from components

--- components/main.js
+++
@@ -1,8 +0,0 @@
-// TODO: Make it compatible with requirejs as well as browserify
-
-module.exports = {
-    alert: require('./alert'),
-    lightbox: require('./lightbox/lightbox'),
-    notifcation: require('./notification'),
-    noscroll: require('./noscroll')
-};
