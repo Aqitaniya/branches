22de7d17cc80c16ad119a4da83b40d5b52724307
Patti Shin <patricia.shin@willowtreeapps.com>
2015-06-16 15:51:36 -0400

    Inserting StoreWatchWrapper ref in main under tools

--- tools/commonjs/main.js
+++ tools/commonjs/main.js
@@ -2,10 +2,11 @@ module.exports = {
     // Modules
     Alert: require('./alert'),
     Lightbox: require('./lightbox'),
+    //?msp (not touching this j.i.c)
     Notifcation: require('./notification'),
     SimpleForm: require('./simple_form'),
     Datepicker: require('./datepicker'),
-
     // Mixins
-    NoScroll: require('./mixins/noscroll')
+    NoScroll: require('./mixins/noscroll'),
+    StoreWatchWrapper: require('./store_watch')
 };
